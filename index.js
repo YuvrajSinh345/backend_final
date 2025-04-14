6fb602f37fedeb2592a85b4cec1f6d76c0b5ef07
Karmpatel18 <karmpatel1203@gmail.com>
2025-04-14 12:25:27 +0530

    d c

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const cors = require("cors");
 app.use(cors()); // Replace with your frontend's URL
 
 app.use(express.json());
-app.use("/", (req, res) => {
-    res.send("Backend is running");
-});
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/quiz", quizRouter);
 app.use("/api/v1/path", careerRouter);
